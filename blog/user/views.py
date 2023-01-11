0dbcd7e72658d7837fce3740afdcd375484945f4
paul <kpasha11@mail.ru>
2023-01-11 20:14:42 +0200

    SQLAlchimi_commit

--- blog/user/views.py
+++ blog/user/views.py
@@ -1,30 +1,29 @@
 from flask import Blueprint, render_template
+from flask_login import login_required
 from werkzeug.exceptions import NotFound
 
+from blog.models import User
+
 user = Blueprint('user', __name__, url_prefix='/users', static_folder='../static')
-USERS = {
-    1: "Gogen",
-    2: "Gurgen",
-    3: "Evgen",
-}
 
 
 @user.route('/')
 def user_list():
-    return render_template('users/list.html', users=USERS)
+    users = User.query.all()
+    return render_template(
+        'users/list.html',
+        users=users,
+    )
 
 
 @user.route('/<int:pk>')
-def get_user_pk(pk: int):
-    user_pk = USERS.get(pk)
-    if user_pk:
-        return render_template('users/details.html', user_name=user_pk)
-    raise NotFound()
+@login_required
+def profile(pk: int):
+    selected_user = User.query.filter_by(id=pk).one_or_none()
+    if not selected_user:
+        raise NotFound(f"User #{pk} doesn't exist!")
 
-
-@user.route('/name/<int:pk>')
-def get_user_name(pk: int):
-    user_pk = USERS.get(pk)
-    if user_pk:
-        return user_pk
-    raise NotFound()
+    return render_template(
+        'users/profile.html',
+        user=selected_user,
+    )
